99766682e76c753447b64c85bd94ff777416af8e
Vinod~ <you@example.com>
2024-06-19 08:40:06 +0530

    1. update route files and paths

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
-import postRoutes from "./routes/postRoutes.js"
+import authRoutes from "./routes/authRoute.js"
+import userRoutes from "./routes/userRoute.js"
+import postRoutes from "./routes/postRoute.js"
 import connectMongoDB from "./data/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 import {v2 as cloudinary} from "cloudinary";
@@ -22,6 +22,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
