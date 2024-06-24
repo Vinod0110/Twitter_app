4adc7f26bec2bc43e02b8adf1afd8cb7db1c5a40
Vinod~ <you@example.com>
2024-06-24 22:29:10 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoute.js"
@@ -19,9 +19,10 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 }); 
+ 
+const __dirname = path.resolve();
 
-
-app.use(express.json({limit:"15mb"})); 
+app.use(express.json({limit:"5mb"})); 
 app.use(express.urlencoded({extended: true})); 
 app.use(cookieParser());
 
@@ -30,6 +31,12 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
